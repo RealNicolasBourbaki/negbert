a350e9d4650ee9fc6dc38898c5ed3b529134f821
NianhengWu <nianheng.wu@ims.uni-stuttgart.de>
2022-09-23 15:50:06 +0200

    remove unnecessary config value

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-SENT_FILE = ""  # the file with all the sentences from which you want to detect negations
-CUES_PRED = ""  # file path where you store all the cues
-SCOPE_PRED = ""  # file path where you store all the scopes
 MODEL_SAVE = "" # root path where you store all the models
 BERT_CONFIG = "models/cue_config.json"
 XNET_CONFIG = "models/xnet_config.json"
